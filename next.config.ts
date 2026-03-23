3d4ffe3461b3333bbbc7c8f79d4fe08fe934463b
bitfalt <75431447+bitfalt@users.noreply.github.com>
2026-03-23 08:52:24 -0600

    Add Next.js project scaffolding

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+import path from 'node:path';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+  turbopack: {
+    root: path.join(__dirname),
+  },
+};
+
+export default nextConfig;
